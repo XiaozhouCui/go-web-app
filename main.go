61b9e24174a56221f0b1b98dff92e8d5579a76af
Joe Cui <xiaozhou.cui@gmail.com>
2022-10-08 17:23:20 +1000

    Another route handler added

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -14,22 +15,42 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is the home page")
 }
 
-// About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
 	sum := addValues(2, 2)
 	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is about page and 2 + 2 is %d", sum))
+}
+
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0, 0.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+		return
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 0.0, f))
 }
 
 func addValues(x, y int) int {
 	return x + y
 }
 
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("cannot divide by 0")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 func main() {
 	// handler function to handle http request
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
-	// listening to localhost:8080
+	// print in terminal
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	// listening to localhost:8080
 	_ = http.ListenAndServe(portNumber, nil)
 }
