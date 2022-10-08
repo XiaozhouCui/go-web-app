a44a9d2f46ca1b24f40208174c81aeccf1d00346
Joe Cui <xiaozhou.cui@gmail.com>
2022-10-08 17:06:43 +1000

    Add more function to handle different routes

--- main.go
+++ main.go
@@ -5,18 +5,31 @@ import (
 	"net/http"
 )
 
+// package level variable
+const portNumber = ":8080"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	// print in browser (http response)
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is about page and 2 + 2 is %d", sum))
+}
+
+func addValues(x, y int) int {
+	return x + y
+}
+
 func main() {
 	// handler function to handle http request
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// print in browser (http response)
-		n, err := fmt.Fprintf(w, "Hello, world!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		// print in terminal
-		fmt.Println(fmt.Sprintf("Number of bytes writter: %d", n))
-	})
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
 	// listening to localhost:8080
-	_ = http.ListenAndServe(":8080", nil)
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 }
