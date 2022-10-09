6d0d6b3f2164166fc68ab43873f9b3bb95820059
Joe Cui <xiaozhou.cui@gmail.com>
2022-10-09 13:35:05 +1000

    Re-organise code and add some styling

---
+++ handlers.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+)
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+}
